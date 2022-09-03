a55f95b60a2933c3f0da19f2603aa11e0f754fef
Vyap26 <vyap@list.ru>
2022-09-03 22:16:13 +0300

    new

---
+++ Carnival.js
@@ -0,0 +1,10 @@
+console.log("WELCOME TO THE CARNIVAL GIFT SHOP!");
+console.log("Hello friend! Thank you for visiting the carnival!");
+console.log("Here's the list of gifts:");
+
+let gifts = ['Teddy Bear', 'Big Red Ball', 'Huge Bear', 'Candy', 'Stuffed Tiger', 'Stuffed Dragon', 'Skateboard',
+    'Toy Car', 'Basketball', 'Scary Mask'];
+
+for (x in gifts) {
+    console.log(gifts[x]);
+}
